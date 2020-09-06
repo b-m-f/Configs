cb741ef864fdf5ee36145af2436613a6e1c46ffd
b-m-f <max@ehlers.berlin>
2020-09-06 08:59:34 +0200

    Fix VsCode config tracking

--- link.sh
+++ link.sh
@@ -1,14 +1,10 @@
 #! /bin/zsh
-<<<<<<< HEAD
-stow -R -t $HOME zsh alacritty-terminal tmux git VsCode nautilus-extensions scripts
-=======
-stow -R -t $HOME zsh alacritty-terminal tmux git nautilus-extensions
+stow -R -t $HOME zsh alacritty-terminal tmux git scripts
 
 
 
 if [ "$(uname)" = "Darwin" ]; then
     stow -R -t $HOME/Library/Application\ Support/ VsCode
 else 
-    stow -R -t $HOME/.config/ VsCode
+    stow -R -t $HOME/.config/ VsCode nautilus-extensions
 fi
->>>>>>> 66b86d6aeed2c55c85a93a759474852c47d71ef2
